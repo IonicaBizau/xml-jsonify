cafa0c71c2f443fa9415d3e27e6cb314a4e9e976
Ionică Bizău <bizauionica@gmail.com>
2016-06-17 12:17:58 +0300

    Docs, functionality

---
+++ DOCUMENTATION.md
@@ -0,0 +1,14 @@
+## Documentation
+
+You can see below the API reference of this module.
+
+### `xmlJsonify(a, b)`
+A liberal XML to JSON converter.
+
+#### Params
+- **Number** `a`: Param descrpition.
+- **Number** `b`: Param descrpition.
+
+#### Return
+- **Number** Return description.
+
--- example/index.js
+++ example/index.js
@@ -2,4 +2,18 @@
 
 const xmlJsonify = require("../lib");
 
-console.log(xmlJsonify());
+// Let's parse a sitemap
+xmlJsonify(`<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>http://www.example.com/</loc>
+  </url>
+  <url>
+    <loc>http://www.example.com/about/</loc>
+  </url>
+  <url>
+    <loc>http://www.example.com/latest-news/</loc>
+  </url>
+</urlset>`, (err, data) => {
+    console.log(err || data);
+});
